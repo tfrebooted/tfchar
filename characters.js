96c2d5ae71c0a66b6f791da5114ab6686bac80f8
DefinitelyKevin <definitelykevin4@Gmail.com>
2024-10-31 22:25:45 -0500

    Create characters.js

---
+++ characters.js
@@ -0,0 +1,54 @@
+const characters = {
+    "optimus": {
+        name: "Optimus Prime",
+        bio: "Noble leader of the Autobots. Originally a data clerk named Orion Pax, he was chosen by the Matrix of Leadership to lead the Autobots in their war against the Decepticons.",
+        altMode: "Freightliner Truck",
+        origin: "Cybertron",
+        height: "28 feet / 8.53 meters",
+        faction: "Autobots",
+        role: "Autobot Leader, Prime",
+        weapons: "Ion Blaster, Energon Axe, Matrix of Leadership"
+    },
+    "bumblebee": {
+        name: "Bumblebee",
+        bio: "One of the smallest and most loved Autobots, Bumblebee makes up for his small size with his big heart and unwavering loyalty to both his Autobot companions and human allies.",
+        altMode: "Chevrolet Camaro",
+        origin: "Cybertron",
+        height: "16 feet / 4.88 meters",
+        faction: "Autobots",
+        role: "Scout, Warrior",
+        weapons: "Plasma Cannon, Stinger Blades"
+    },
+    "megatron": {
+        name: "Megatron",
+        bio: "Former gladiator turned tyrannical leader of the Decepticons. His ruthless ambition and overwhelming power make him one of the most feared beings in the galaxy.",
+        altMode: "Cybertronian Jet / Tank",
+        origin: "Cybertron",
+        height: "35 feet / 10.67 meters",
+        faction: "Decepticons",
+        role: "Decepticon Leader",
+        weapons: "Fusion Cannon, Energon Mace, Dark Energon Powers"
+    },
+    "starscream": {
+        name: "Starscream",
+        bio: "The treacherous second-in-command of the Decepticons. While a skilled warrior and scientist, his overwhelming ambition and tendency to plot against Megatron often leads to his downfall.",
+        altMode: "F-15 Eagle Fighter Jet",
+        origin: "Cybertron",
+        height: "26 feet / 7.92 meters",
+        faction: "Decepticons",
+        role: "Air Commander, Second-in-Command",
+        weapons: "Null Rays, Missile Launchers, Cluster Bombs"
+    },
+    "jazz": {
+        name: "Jazz",
+        bio: "The cool, calm, and cultural-loving special operations agent. Jazz's adaptability and laid-back attitude make him one of the most effective Autobot officers.",
+        altMode: "Porsche 935",
+        origin: "Cybertron",
+        height: "15 feet / 4.57 meters",
+        faction: "Autobots",
+        role: "Special Operations, First Lieutenant",
+        weapons: "Photon Rifle, Flame Thrower, Sound and Light Disruption Equipment"
+    }
+};
+
+export default characters;
