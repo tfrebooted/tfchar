e8b82ab903c1d35423823c4d522f1dcc485b04c9
tfrebooted <definitelykevin4@Gmail.com>
2024-11-10 08:46:19 -0600

    Update characters.js

--- characters.js
+++ characters.js
@@ -1,53 +1,13 @@
 const characters = {
-    "optimus-prime": {
-        name: "Optimus Prime",
-        bio: "Noble leader of the Autobots. Originally a data clerk named Orion Pax, he was chosen by the Matrix of Leadership to lead the Autobots in their war against the Decepticons.",
-        altMode: "Freightliner Truck",
-        origin: "Cybertron",
-        height: "28 feet / 8.53 meters",
-        faction: "Autobots",
-        role: "Autobot Leader, Prime",
-        weapons: "Ion Blaster, Energon Axe, Matrix of Leadership"
-    },
-    "bumblebee": {
-        name: "Bumblebee",
-        bio: "One of the smallest and most loved Autobots, Bumblebee makes up for his small size with his big heart and unwavering loyalty to both his Autobot companions and human allies.",
-        altMode: "Chevrolet Camaro",
-        origin: "Cybertron",
-        height: "16 feet / 4.88 meters",
-        faction: "Autobots",
-        role: "Scout, Warrior",
-        weapons: "Plasma Cannon, Stinger Blades"
-    },
-    "megatron": {
-        name: "Megatron",
-        bio: "Former gladiator turned tyrannical leader of the Decepticons. His ruthless ambition and overwhelming power make him one of the most feared beings in the galaxy.",
-        altMode: "Cybertronian Jet / Tank",
-        origin: "Cybertron",
-        height: "35 feet / 10.67 meters",
-        faction: "Decepticons",
-        role: "Decepticon Leader",
-        weapons: "Fusion Cannon, Energon Mace, Dark Energon Powers"
-    },
-    "starscream": {
-        name: "Starscream",
-        bio: "The treacherous second-in-command of the Decepticons. While a skilled warrior and scientist, his overwhelming ambition and tendency to plot against Megatron often leads to his downfall.",
-        altMode: "F-15 Eagle Fighter Jet",
-        origin: "Cybertron",
-        height: "26 feet / 7.92 meters",
-        faction: "Decepticons",
-        role: "Air Commander, Second-in-Command",
-        weapons: "Null Rays, Missile Launchers, Cluster Bombs"
-    },
-    "jazz": {
-        name: "Jazz",
-        bio: "The cool, calm, and cultural-loving special operations agent. Jazz's adaptability and laid-back attitude make him one of the most effective Autobot officers.",
-        altMode: "Porsche 935",
-        origin: "Cybertron",
-        height: "15 feet / 4.57 meters",
-        faction: "Autobots",
-        role: "Special Operations, First Lieutenant",
-        weapons: "Photon Rifle, Flame Thrower, Sound and Light Disruption Equipment"
+    "acid-storm": {
+        name: "Acid Storm",
+        bio: "Dark agent of Unicron. Born from the darkness into the Destructicons to fight against the Prines, he rose once more to become a key Seeker in the Great War.",
+        altMode: "Cybertronian Jet",
+        origin: "Unicron",
+        height: "22 feet / 6.7 meters",
+        faction: "Destructicons",
+        role: "Destructicon Rainmaker",
+        weapons: "Dual Null Rays, Weather Manipulation (Acidic), Supercharged Acidic Armor"
     }
 };
 
