01c29af9e61b9675b84fe378f4651817d847f41d
tfrebooted <definitelykevin4@Gmail.com>
2024-11-11 21:21:52 -0600

    Update characters.js

--- characters.js
+++ characters.js
@@ -1,7 +1,7 @@
 const characters = {
     "acid-storm": {
         name: "Acid Storm",
-        bio: "Dark agent of Unicron. Born from the darkness into the Destructicons to fight against the Prines, he rose once more to become a key Seeker in the Great War.",
+        bio: "Dark agent of Unicron. Born from the darkness into the Destructicons to fight against the Primes, he rose once more to become a key Seeker in the Great War.",
         altMode: "Cybertronian Jet",
         origin: "Unicron",
         height: "22 feet / 6.7 meters",
